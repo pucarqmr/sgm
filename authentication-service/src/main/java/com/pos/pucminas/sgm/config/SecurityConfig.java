7b6742f085e37975d541a5452d1a1b84147a20a6
Rodrigo de Araujo <rodrigoufu@gmail.com>
2021-03-16 20:18:05 -0300

    Authentication service

---
+++ authentication-service/src/main/java/com/pos/pucminas/sgm/config/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.pos.pucminas.sgm.config;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.pos.pucminas.sgm.security.common.JwtAuthenticationConfig;
+import com.pos.pucminas.sgm.security.common.JwtUsernamePasswordAuthenticationFilter;
+
+/**
+ * Config login authentication.
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired JwtAuthenticationConfig config;
+
+    @Bean
+    public JwtAuthenticationConfig jwtConfig() {
+        return new JwtAuthenticationConfig();
+    }
+
+    @Autowired
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	PasswordEncoder encoder = 
+          PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    	auth
+          .inMemoryAuthentication()
+          .withUser("user")
+          .password(encoder.encode("user"))
+          .roles("USER")
+          .and()
+          .withUser("admin")
+          .password(encoder.encode("admin"))
+          .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .logout().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .anonymous()
+                .and()
+                    .exceptionHandling().authenticationEntryPoint(
+                            (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                    .addFilterAfter(new JwtUsernamePasswordAuthenticationFilter(config, authenticationManager()),
+                            UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                    .antMatchers(config.getUrl()).permitAll()
+                    .anyRequest().authenticated();
+    }
+}
+
