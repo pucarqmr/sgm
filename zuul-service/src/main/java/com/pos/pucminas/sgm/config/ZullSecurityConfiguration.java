e2bc5bf759da5283785a1f35138f2b6b9ddd5122
Rodrigo de Araujo <rodrigoufu@gmail.com>
2021-03-16 20:21:52 -0300

    API Gateway

---
+++ zuul-service/src/main/java/com/pos/pucminas/sgm/config/ZullSecurityConfiguration.java
@@ -0,0 +1,47 @@
+package com.pos.pucminas.sgm.config;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.pos.pucminas.sgm.security.common.JwtAuthenticationConfig;
+import com.pos.pucminas.sgm.security.common.JwtTokenAuthenticationFilter;
+
+@EnableWebSecurity
+public class ZullSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    private JwtAuthenticationConfig config;
+
+    @Bean
+    public JwtAuthenticationConfig jwtConfig() {
+        return new JwtAuthenticationConfig();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .logout().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .anonymous()
+                .and()
+                    .exceptionHandling().authenticationEntryPoint(
+                            (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                    .addFilterAfter(new JwtTokenAuthenticationFilter(config),
+                            UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                    .antMatchers(config.getUrl()).permitAll()
+                    .antMatchers("/api/servicos/**").hasRole("ADMIN");
+    }
+
+}
